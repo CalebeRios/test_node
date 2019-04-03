3688347e46908be3653a9bec211f1bbd9cf6cfd6
CalebeRios <calebe_rios@hotmail.com>
2019-04-02 22:06:40 -0300

    t

--- src/test/indexTest.js
+++ src/test/indexTest.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../index');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
 
-const should = chai.should();
+let should = chai.should();
 
 chai.use(chaiHttp);
 
