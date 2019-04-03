90548ccd3fdf196ef3aedae3b7d11e39529d190e
CalebeRios <calebe_rios@hotmail.com>
2019-04-02 21:53:05 -0300

    teste

---
+++ src/test/indexTest.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../index');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('test', () => {
+  it('get', (done) => {
+    chai.request(server).get('/').end((err, res) => {
+      res.should.have.status(200);
+      done();
+    });
+  });
+});
